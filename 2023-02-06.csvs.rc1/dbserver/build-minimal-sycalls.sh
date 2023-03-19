7a1d6df54026067d1af0af8082df7dcf74053bcf
Patrick <52357205+leo031938@users.noreply.github.com>
2023-03-19 20:42:15 +0000

    Add files via upload

---
+++ 2023-02-06.csvs.rc1/dbserver/build-minimal-sycalls.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# find minimal syscalls by removing from default syscalls
+
+rm list-of-min-syscalls
+while read s
+do
+  echo "$s  being removed from moby-default.json"
+  grep -v "\"${s}\"" ./moby-default.json > tmp.json
+  docker run -d --rm --net u2126529/csvs2023_n --ip 203.0.113.201 --hostname db.cyber23.test -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple" -e MYSQL_DATABASE="csvs23db" --name u2126529_csvs2023-db_c --security-opt seccomp:tmp.json u2126529/csvs2023-db_i 
+
+  # Wait for container to start
+  sleep 5s
+  
+  # Send GET request
+  Get_response=$(curl --write-out "%{http_code}" --silent --output /dev/null --max-time 5 http://localhost/index.php)
+ 
+  # Send POST request
+  Post_response=$(curl --write-out "%{http_code}" --silent --output /dev/null --max-time 5 -X POST -d '{"Username":"value1", "suggestion":"value2"}' http://localhost/action.php)
+
+  if [[ $Get_response -eq 200 && $Post_response -eq 302 ]]; then
+    echo "$s is running properly, do nothing"
+	
+  else 
+    echo "$s is not success, adding to list-of-min-syscalls"
+    echo $s >> list-of-min-syscalls
+
+  fi
+
+  # kill docker
+  docker kill u2126529_csvs2023-web_c
+
+done < ./moby-syscalls
+
+
+
