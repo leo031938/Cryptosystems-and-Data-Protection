f163fe36dd3af69426cb63532402067612f3c31d
Patrick <52357205+leo031938@users.noreply.github.com>
2023-03-20 22:45:13 +0000

    Add files via upload

--- 2023-02-06.csvs.rc1_20230320/dbserver/build-minimal-sycalls.sh
+++ 2023-02-06.csvs.rc1_20230320/dbserver/build-minimal-sycalls.sh
@@ -6,16 +6,22 @@ while read s
 do
   echo "$s  being removed from moby-default.json"
   grep -v "\"${s}\"" ./moby-default.json > tmp.json
+
   docker run -d --rm --net u2126529/csvs2023_n --ip 203.0.113.201 --hostname db.cyber23.test -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple" -e MYSQL_DATABASE="csvs23db" --name u2126529_csvs2023-db_c --security-opt seccomp:tmp.json u2126529/csvs2023-db_i 
 
   # Wait for container to start
-  sleep 5s
-  
+  sleep 10s
+  echo "sleep"
+  # prepare for database
+  timeout 3 docker exec -i u2126529_csvs2023-db_c mysql -uroot -pCorrectHorseBatteryStaple < sqlconfig/csvs23db.sql  
+  echo "prepare for database"
   # Send GET request
   Get_response=$(curl --write-out "%{http_code}" --silent --output /dev/null --max-time 5 http://localhost/index.php)
  
   # Send POST request
-  Post_response=$(curl --write-out "%{http_code}" --silent --output /dev/null --max-time 5 -X POST -d '{"Username":"value1", "suggestion":"value2"}' http://localhost/action.php)
+  value=$RANDOM
+  echo $value
+  Post_response=$(curl --write-out "%{http_code}" --silent --output /dev/null --max-time 5 -X POST -d "fullname=$value&suggestion=$value" http://localhost/action.php)
 
   if [[ $Get_response -eq 200 && $Post_response -eq 302 ]]; then
     echo "$s is running properly, do nothing"
@@ -27,7 +33,7 @@ do
   fi
 
   # kill docker
-  docker kill u2126529_csvs2023-web_c
+  docker kill u2126529_csvs2023-db_c
 
 done < ./moby-syscalls
 
--- 2023-02-06.csvs.rc1_20230320/webserver/configfiles/docker-entrypoint.sh
+++ 2023-02-06.csvs.rc1_20230320/webserver/configfiles/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 mkdir -p /run/php-fpm/
 /usr/sbin/nginx
 /usr/sbin/php-fpm -F
